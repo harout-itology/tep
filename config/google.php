bfd6615d23d21d0f30dcbe081a38d0a33e8560eb
Harout Koja <harout.itology@gmail.com>
2017-10-11 00:58:54 +0400

    google

--- config/google.php
+++ config/google.php
@@ -2,10 +2,10 @@
 
 
 return [
-    'setApplicationName' => '',
-    'setClientId' => '',
-    'setClientSecret' => '',
-    'setDeveloperKey' => '',
+    'setApplicationName' => 'tep-v1',
+    'setClientId' => '16239107095-v6k36cujp6oqpapub0dmj1dlet6k6tb5.apps.googleusercontent.com',
+    'setClientSecret' => 'cRC4G9aB2kMX_T2HMla-xGCq',
+    'setDeveloperKey' => 'AIzaSyAFW6ifz4HBeEU1-ZDHUgSd8eC_Krq8eB4',
     'google_redirect_url' => 'google-oauth2callback',
     'googleSetScopes' => [
         'https://www.googleapis.com/auth/plus.me',
