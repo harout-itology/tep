879496416eefc7555ea68f5c2022a7e7f454263e
harout-itology <harout.itology@gmail.com>
2017-10-11 13:46:42 +0400

    bug fix

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -51,8 +51,7 @@ class LoginController extends Controller
         $authUrl = parent::googleconfig();
         $google = $authUrl->createAuthUrl();
 
-        $linkedin = LinkedIn::getLoginUrl();
-        $linkedin = str_replace('login','linkedin-oauth2callback',$linkedin);
+        $linkedin  = 'http://wirelessinfrastructuredata.com/linkedin-oauth2callback'; // LinkedIn::getLoginUrl();
 
         return view('auth.login',['google'=>$google,'linkedin'=>$linkedin]);
     }
