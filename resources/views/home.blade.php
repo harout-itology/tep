4c6178c73245347d0b3f64333a3b73bc92b8bd5b
Harout Koja <harout.itology@gmail.com>
2017-10-12 17:29:16 +0400

    Home page view

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,18 +1,130 @@
 @extends('layouts.app')
 
 @section('head')
+
+    <style>
+        body{
+            background-image: none;
+        }
+        .dataTables_length{
+            margin:0 18%;
+        }
+    </style>
+
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.10.16/css/jquery.dataTables.min.css" >
+    <link rel="stylesheet" href="https://cdn.datatables.net/buttons/1.4.2/css/buttons.dataTables.min.css" >
+
 @endsection
 
 @section('content')
-    <div class="container">
+    <div class="container" style="width: 100%">
         <div class="row  ">
             <div class="col-md-12 ">
                 <div class="panel panel-default">
                     <div class="panel-body">
 
-                        <h2> Map</h2>
+                        <ul class="nav nav-tabs">
+                            <li class="active"><a data-toggle="tab" href="#menu1"><i class="fa fa-bars" aria-hidden="true"></i> &nbsp;  List View</a></li>
+                            <li><a data-toggle="tab" href="#menu2"><i class="fa fa-map-marker" aria-hidden="true"></i> &nbsp; Map View</a></li>
+                        </ul><br>
 
-                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3046.968286887214!2d44.46709991488313!3d40.209768379389836!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x406a97deb99e3185%3A0x8d8071ec01177d6!2sMegaProgramming!5e0!3m2!1sen!2s!4v1507737322159" width="100%" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+                        <div class="tab-content">
+                            <div id="menu1" class="tab-pane fade in active">
+
+                                <table id="example" class="display nowrap" cellspacing="0" width="100%">
+                                    <thead>
+                                    <tr>
+                                        <th>Tower ID</th>
+                                        <th>Site Name</th>
+                                        <th>Address</th>
+                                        <th>City</th>
+                                        <th>Country</th>
+                                        <th>Height</th>
+                                        <th>Infication</th>
+                                        <th>Owner</th>
+                                    </tr>
+                                    </thead>
+                                    <tfoot>
+                                    <tr>
+                                        <th>Tower ID</th>
+                                        <th>Site Name</th>
+                                        <th>Address</th>
+                                        <th>City</th>
+                                        <th>Country</th>
+                                        <th>Height</th>
+                                        <th>Infication</th>
+                                        <th>Owner</th>
+                                    </tr>
+                                    </tfoot>
+                                    <tbody>
+                                    <tr>
+                                        <td>Tiger Nixon1</td>
+                                        <td>System Architect1</td>
+                                        <td>Edinburgh1</td>
+                                        <td>611</td>
+                                        <td>2011/04/25</td>
+                                        <td>$320,800</td>
+                                        <td>$320,800</td>
+                                        <td>$320,800</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Tiger Nixon2</td>
+                                        <td>System Architect2</td>
+                                        <td>Edinburgh2</td>
+                                        <td>61</td>
+                                        <td>2011/04/25</td>
+                                        <td>$320,800</td>
+                                        <td>$320,800</td>
+                                        <td>$320,800</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Tiger Nixon3</td>
+                                        <td>System Architect3</td>
+                                        <td>Edinburgh3</td>
+                                        <td>61</td>
+                                        <td>2011/04/25</td>
+                                        <td>$320,800</td>
+                                        <td>$320,800</td>
+                                        <td>$320,800</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Tiger Nixon4</td>
+                                        <td>System Architect4</td>
+                                        <td>Edinburgh4</td>
+                                        <td>61</td>
+                                        <td>2011/04/25</td>
+                                        <td>$320,800</td>
+                                        <td>$320,800</td>
+                                        <td>$320,800</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Tiger Nixon2</td>
+                                        <td>System Architect2</td>
+                                        <td>Edinburgh2</td>
+                                        <td>61</td>
+                                        <td>2011/04/25</td>
+                                        <td>$320,800</td>
+                                        <td>$320,800</td>
+                                        <td>$320,800</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Tiger Nixon1</td>
+                                        <td>System Architect1</td>
+                                        <td>Edinburgh1</td>
+                                        <td>611</td>
+                                        <td>2011/04/25</td>
+                                        <td>$320,800</td>
+                                        <td>$320,800</td>
+                                        <td>$320,800</td>
+                                    </tr>
+                                    </tbody>
+                                </table>
+
+                            </div>
+                            <div id="menu2" class="tab-pane fade">
+                                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d6593268.507391775!2d-100.3032860467984!3d36.20621560613941!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x54eab584e432360b%3A0x1c3bb99243deb742!2sUnited+States!5e0!3m2!1sen!2s!4v1507806659594" width="100%" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+                            </div>
+                        </div>
 
                     </div>
                 </div>
@@ -22,4 +134,39 @@
 @endsection
 
 @section('foot')
+
+    <script src="https://cdn.datatables.net/1.10.16/js/jquery.dataTables.min.js"></script>
+    <script src="https://cdn.datatables.net/1.10.16/js/dataTables.bootstrap.min.js"></script>
+    <script src="https://cdn.datatables.net/buttons/1.4.2/js/dataTables.buttons.min.js"></script>
+    <script src="//cdn.datatables.net/buttons/1.4.2/js/buttons.colVis.min.js"></script>
+    <script src="//cdn.datatables.net/buttons/1.4.2/js/buttons.flash.min.js"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.32/pdfmake.min.js"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.32/vfs_fonts.js"></script>
+    <script src="//cdn.datatables.net/buttons/1.4.2/js/buttons.html5.min.js"></script>
+    <script src="//cdn.datatables.net/buttons/1.4.2/js/buttons.print.min.js"></script>
+
+    <script>
+        $(document).ready(function() {
+            $('#example').DataTable( {
+                dom: '<"top"Bfl>rt<"bottom"ip>',
+                "scrollX": true,
+                buttons: [ 'colvis', 'csv', 'pdf', 'print' ],
+                initComplete: function () {
+                    this.api().columns().every( function () {
+                        var column = this;
+                        var select = $('<select class="form-control" ><option value="">All</option></select>').appendTo( $(column.footer()).empty() ).on( 'change', function () {
+                            var val = $.fn.dataTable.util.escapeRegex($(this).val());
+                            column.search( val ? '^'+val+'$' : '', true, false ).draw();
+                         } );
+                        column.data().unique().sort().each( function ( d, j ) {
+                            select.append( '<option value="'+d+'">'+d+'</option>' )
+                        } );
+                    } );
+                }
+            } );
+            $('.dt-button').addClass('btn btn-default').removeClass('dt-button');
+        } );
+    </script>
+
 @endsection
