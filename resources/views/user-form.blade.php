4ecb6abd2fc2f9cfe1d199cf3bbcab319b41160f
Harout Koja <harout.itology@gmail.com>
2017-10-13 10:55:18 +0400

    fix bugs

--- resources/views/user-form.blade.php
+++ resources/views/user-form.blade.php
@@ -20,7 +20,7 @@
                             {{ method_field('PUT') }}
 
                             <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
-                                <label for="name" class="col-md-4 control-label">Name</label>
+                                <label for="name" class="col-md-4 control-label">Name *</label>
 
                                 <div class="col-md-6">
                                     <input id="name" type="text" class="form-control" name="name" value="{{ old('name') ? old('name')  :  $user->name }}" required autofocus>
@@ -34,7 +34,7 @@
                             </div>
 
                             <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                                <label for="email" class="col-md-4 control-label">E-Mail Address</label>
+                                <label for="email" class="col-md-4 control-label">E-Mail Address *</label>
 
                                 <div class="col-md-6">
                                     <input id="email" type="email" class="form-control" name="email" value="{{ old('email') ? old('email')  :  $user->email }}" required>
