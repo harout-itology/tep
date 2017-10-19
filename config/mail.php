d5dbb5d2973e21aaa13c9eec153bee580062487f
Harout Koja <harout.itology@gmail.com>
2017-10-19 10:37:18 +0400

    mail bug

--- config/mail.php
+++ config/mail.php
@@ -56,7 +56,7 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'wirelessinfrastructuredata@gmail.com'),
+        'address' => env('MAIL_FROM_ADDRESS', 'test@armgento.com'),
         'name' => env('MAIL_FROM_NAME', 'wirelessinfrastructuredata'),
     ],
 
